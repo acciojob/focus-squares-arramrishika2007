74af6c6172af754c85d735dca1e74ab6058433ff
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-07-04 19:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+
+  square.addEventListener("mouseenter", () => {
+
+    squares.forEach(box => {
+      if (box === square) {
+        box.style.backgroundColor = "#E6E6FA"; // Lavender
+      } else {
+        box.style.backgroundColor = "#6F4E37"; // Coffee
+      }
+    });
+
+  });
+
+  square.addEventListener("mouseleave", () => {
+
+    squares.forEach(box => {
+      box.style.backgroundColor = "#E6E6FA";
+    });
+
+  });
+
+});
