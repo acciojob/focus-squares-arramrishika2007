8e571ffe9c4cc36a1fc123219b1d69bb2ece18bb
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-07-04 19:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-//your JS code here. If required.
 const squares = document.querySelectorAll(".square");
 
 squares.forEach(square => {
 
-  square.addEventListener("mouseenter", () => {
+  square.addEventListener("mouseover", () => {
 
     squares.forEach(box => {
       if (box === square) {
-        box.style.backgroundColor = "#E6E6FA"; // Lavender
+        box.style.backgroundColor = "#E6E6FA";
       } else {
-        box.style.backgroundColor = "#6F4E37"; // Coffee
+        box.style.backgroundColor = "#6F4E37";
       }
     });
 
   });
 
-  square.addEventListener("mouseleave", () => {
+  square.addEventListener("mouseout", () => {
 
     squares.forEach(box => {
       box.style.backgroundColor = "#E6E6FA";
